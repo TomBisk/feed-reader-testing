9d891375db455ace05a952b36d03cbd0d500006b
Tomasz Bochenek <bochenekt@gmail.com>
2018-08-02 21:39:38 +0200

    feat: add empty 'The menu' suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,8 +52,10 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
+    /* Test suite which contains set of tests for the Menu,
+	 *to tests its default state and when it's clicked.
+	 */
+	describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -65,7 +67,7 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+	});
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
